c2312a40c5f777f0d1fac31d919916e3201b825f
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-06-14 14:49:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Import module
 import discord
 from discord.ext import commands
-from replit import db
+import config
 import os
 import requests
 from keep_alive import keep_alive
@@ -12,12 +12,11 @@ intents = discord.Intents.default()
 intents.guilds=True
 intents.messages=True
 intents.members=True
-client=commands.Bot(command_prefix=["gv!", "Gv!", "GV!", "<@909367670833561600> "], intents=intents)
+client=commands.Bot(command_prefix=config["prefix"], intents=intents)
 DiscordComponents(client)
 #load cogs
 def load():
-    cogs=[x.replace(".py", "") for x in os.listdir("Cogs")]
-    cogs.remove("__pycache__")
+    cogs=[x.replace(".py", "") for x in os.listdir("Cogs")
     for cog in cogs:
         try:
             client.load_extension("Cogs."+cog)
@@ -27,28 +26,6 @@ def load():
 async def on_ready():
     load()
     print(client.user)
-@client.command()
-async def rint(ctx,*,command):
-    if ctx.author.id!=720900711260487681:
-        return
-    try:
-        pr=eval(command)
-    except Exception as ex:
-        pr=f"{type(ex).__name__}: {ex.args[0]}"
-    await ctx.reply(pr)
-@client.command()
-async def run(ctx, *,command):
-    if ctx.author.id==720900711260487681:
-        try:
-            exec(command)
-        except Exception as ex:
-            await ctx.reply(f"{type(ex).__name__}: {ex.args[0]}")
-        else:
-            await ctx.message.add_reaction("✅")
 #run client 
-if requests.get("https://verify.caiwinson.repl.co").text!="200":
-    keep_alive()
-    try:
-        client.run(db["token"])
-    except Exception as exp:
-        a=exp
+if __name__=='__main__':
+    client.run(config["token"])
